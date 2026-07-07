cb303dc29f567a79d84131bd1072d3b0852b79a0
Makarov Nikolay <giper.pipec@gmail.com>
2026-07-07 19:30:44 +0300

    feat: add SecurityConfig

--- user-service/src/main/java/com/notification/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/notification/userservice/config/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.notification.userservice.config;
 
+import com.notification.userservice.security.JwtAuthFilter;
+import com.notification.userservice.security.JwtCore;
+import com.notification.userservice.security.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter(JwtCore jwtCore, UserDetailsServiceImpl userDetailsService) {
+        return new JwtAuthFilter(jwtCore, userDetailsService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).build();
+    }
+
 }
